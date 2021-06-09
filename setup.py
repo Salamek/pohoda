781b65b316d2cf959abbd0b62af5ec0494d77dae
Adam Schubert <adam.schubert@sg1-game.net>
2021-06-09 16:30:32 +0200

    New version 1.0.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pohoda',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'pohoda': ['py.typed']},
     install_requires=[
